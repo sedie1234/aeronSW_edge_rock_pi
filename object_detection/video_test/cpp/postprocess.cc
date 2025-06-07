3d638a6a57b1e16df2814037b1007bb71dc6d486
dladb0821 <dlop4655@naver.com>
2025-06-07 06:24:07 +0200

    OBJ_CLASS_NUM, LABEL_NALE_TXT_PATH 수정

--- object_detection/video_test/cpp/postprocess.cc
+++ object_detection/video_test/cpp/postprocess.cc
@@ -23,8 +23,9 @@
 
 #include <set>
 #include <vector>
-// #define LABEL_NALE_TXT_PATH "./model/coco_80_labels_list.txt"
-#define LABEL_NALE_TXT_PATH "../../object_detection/video_test/model/coco_80_labels_list.txt"
+// #define LABEL_NALE_TXT_PATH "../../object_detection/video_test/model/coco_80_labels_list.txt"
+#define LABEL_NALE_TXT_PATH "../../object_detection/video_test/model/uam_5_labels_list.txt"
+// #define LABEL_NALE_TXT_PATH "../model/uam_5_labels_list.txt"
 
 static char *labels[OBJ_CLASS_NUM];
 
